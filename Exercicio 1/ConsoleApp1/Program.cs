1d30b7baa1c8aaf77f2bf98e31a91bc93e242c30
rika500 <56159011+rika500@users.noreply.github.com>
2019-10-04 15:23:07 +0100

    Add files via upload

---
+++ Exercicio 1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+
+            Random r = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(1, 20);
+            }
+
+
+            Console.WriteLine(Comparar(array, 2));
+            
+            
+        }
+
+        
+
+
+
+        public static int Comparar(int [] a, int b)
+        {
+            int c = 0;
+            for (int i = 0; i < a.Length; i++)
+
+            {
+                if( a[i] == b)
+                {
+                    c++;
+                }
+                
+            }
+            return c;
+        }
+    }
+}
